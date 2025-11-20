29b848e698f87729d39aa18166cf0c2344a16465
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-20 12:14:10 +0700

    Menambahkan libs dan import project  minggu ke 5

---
+++ app/src/main/java/com/example/quest6_094/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.quest6_094
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quest6_094.ui.theme.Quest6_094Theme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Quest6_094Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    DataApp(
+                        modifier = Modifier
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Quest6_094Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/quest6_094/Navigasi.kt
@@ -0,0 +1,54 @@
+package com.example.quest6_094
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_094.view.FormIsian
+import com.example.quest6_094.view.TampilData
+
+enum class Navigasi{
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding( isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map {id -> konteks.resources.getString
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable ( route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulirku(
+    navController: NavController
+){
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
+}
