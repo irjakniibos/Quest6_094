5639902c4eb10e64036aeac2f16455a5838e129a
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-20 12:26:09 +0700

    Menambahkan dependensi Navigation dan Lifecycle (ViewModel & Runtime) untuk mendukung pengembangan berbasis Jetpack Compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.navigation.compose)
+
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
